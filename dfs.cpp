823105d69f3ab2f49acb546d7645f1a77c78fc7e
Vinay <vinaycode7@gmail.com>
2023-02-02 15:01:53 +0000

    Added build_graph

--- dfs.cpp
+++ dfs.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
-
-main()
+map<int, vector<int>> build_graph(vector<vector<int>> edges)
 {
-    int n = 3;
-    int graph[n][n];
-    for (int i = 0; i < n; i++)
+    map<int, vector<int>> graph;
+
+    for (int i = 0; i < edges.size(); i++)
     {
-        for (int j = 0; j < n; j++)
-            graph[i][j] = 0;
+        int a = edges[i][0];
+        int b = edges[i][1];
+
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
 
+    return graph;
+}
+
+int main()
+{
+    cout << "Enter number of edges: ";
+    int n;
+    cin >> n;
+
+    vector<vector<int>> edges;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-            cout << graph[i][j] << " ";
+        int a, b;
+        cin >> a >> b;
+        edges.push_back(vector<int>());
+        edges[i].push_back(a);
+        edges[i].push_back(b);
+    }
+
+    map<int, vector<int>> graph = build_graph(edges);
+
+    for (map<int, vector<int>>::iterator data = graph.begin(); data != graph.end(); data++)
+    {
+        cout << data->first << " : ";
+        for (int j = 0; j < data->second.size(); j++)
+            cout << data->second[j] << " ";
         cout << endl;
     }
 }
