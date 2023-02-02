f141e32e1cc90d1fe5c68fecad777568c73a292f
Vinay <vinaycode7@gmail.com>
2023-02-02 22:08:17 +0530

    graph class

--- dfs.cpp
+++ dfs.cpp
@@ -3,31 +3,48 @@
 #include <vector>
 using namespace std;
 
-map<int, vector<int>> build_graph(vector<vector<int>> edges)
+class Graph
 {
+public:
     map<int, vector<int>> graph;
 
-    for (int i = 0; i < edges.size(); i++)
+    Graph(vector<vector<int>> edges)
     {
-        int a = edges[i][0];
-        int b = edges[i][1];
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int a = edges[i][0];
+            int b = edges[i][1];
 
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
     }
 
-    return graph;
-}
+    void print()
+    {
+        for (auto data = graph.begin(); data != graph.end(); data++)
+        {
+            int src = data->first;
+            vector<int> nei = data->second;
 
-int main()
+            cout << src << ": ";
+            for (int j = 0; j < nei.size(); j++)
+                cout << nei[j] << " ";
+            cout << endl;
+        }
+    }
+};
+
+vector<vector<int>> input_edges()
 {
     cout << "Enter number of edges: ";
     int n;
     cin >> n;
-
     vector<vector<int>> edges;
+
     for (int i = 0; i < n; i++)
     {
+        cout << "Enter edge: ";
         int a, b;
         cin >> a >> b;
         edges.push_back(vector<int>());
@@ -35,13 +52,14 @@ int main()
         edges[i].push_back(b);
     }
 
-    map<int, vector<int>> graph = build_graph(edges);
+    return edges;
+}
 
-    for (map<int, vector<int>>::iterator data = graph.begin(); data != graph.end(); data++)
-    {
-        cout << data->first << " : ";
-        for (int j = 0; j < data->second.size(); j++)
-            cout << data->second[j] << " ";
-        cout << endl;
-    }
+int main()
+{
+    vector<vector<int>> edges = input_edges();
+    
+    Graph graph = Graph(edges);
+    graph.print();
+    
 }
