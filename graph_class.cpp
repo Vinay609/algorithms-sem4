bca8883919d54eed806529916bafef9bc7c25723
Vinay <vinaycode7@gmail.com>
2023-02-04 12:53:02 +0530

    changes

--- graph_class.cpp
+++ graph_class.cpp
@@ -42,22 +42,3 @@ public:
         }
     }
 };
-
-
-int main()
-{
-    Graph graph;
-
-    cout << "Enter number of edges: ";
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Enter edge: ";
-        int a, b;
-        cin >> a >> b;
-        graph.add_edge(a, b);
-    }
-
-    graph.print();
-}
