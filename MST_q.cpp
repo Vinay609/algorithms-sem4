89d4c9cfa89ba18f8477917f938df1529a56ac1e
Vinay <vinaycode7@gmail.com>
2023-02-26 02:40:34 +0530

    mst_implemented

---
+++ MST_q.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define V 9
+#define INF 999
+
+void primMST(int graph[V][V], int src)
+{
+    int parent[V];
+    int key[V];
+    bool visited[V];
+
+    for (int i = 0; i < V; i++)
+    {
+        parent[i] = -1;
+        key[i] = INF;
+        visited[i] = false;
+    }
+
+    key[src] = 0;
+
+    priority_queue<pair<int, int>> pq;
+    pq.push({0, src});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (visited[u])
+            continue;
+        visited[u] = true;
+
+        for (int v = 0; v < V; v++)
+        {
+            int weight = graph[u][v];
+
+            if (graph[u][v] && !visited[v] && weight < key[v])
+            {
+                parent[v] = u;
+                key[v] = weight;
+                pq.push({-weight, v});
+            }
+        }
+    }
+
+    for (int i = 1; i < V; i++)
+        printf("%c - %c\n", 'a' + i, 'a' + parent[i]);
+}
+
+int main()
+{
+    /*
+    int graph[V][V] = {{0, 2, 0, 6, 0},
+                       {2, 0, 3, 8, 5},
+                       {0, 3, 0, 0, 7},
+                       {6, 8, 0, 0, 9},
+                       {0, 5, 7, 9, 0}};
+    V = 5
+    */
+
+    int graph[V][V] = {
+        // a b c  d  e  f  g  h  i
+        {0, 4, 0, 0, 0, 0, 0, 8, 0},   // a
+        {4, 0, 8, 0, 0, 0, 0, 11, 0},  // b
+        {0, 8, 0, 7, 0, 4, 0, 0, 2},   // c
+        {0, 0, 7, 0, 9, 14, 0, 0, 0},  // d
+        {0, 0, 0, 9, 0, 10, 0, 0, 0},  // e
+        {0, 0, 4, 14, 10, 0, 2, 0, 0}, // f
+        {0, 0, 0, 0, 0, 2, 0, 1, 6},   // g
+        {8, 11, 0, 0, 0, 0, 1, 0, 7},  // h
+        {0, 0, 2, 0, 0, 0, 6, 7, 0},   // i
+    };
+
+    primMST(graph, 0);
+}
